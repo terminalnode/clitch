248adc4e92f8e3be4c8503a4b40800e4cb36d134
Alexander Rundberg <alex@sect.tech>
2024-03-21 20:25:52 +0100

    add continuous and overwrite to clock

--- src/subcommands/clock/args.rs
+++ src/subcommands/clock/args.rs
@@ -3,4 +3,12 @@ use clap::Args;
 /// Show the current time
 #[derive(Args, Debug)]
 #[command(name = "clock", about)]
-pub struct ClockArgs {}
+pub struct ClockArgs {
+    /// If set, print the remaining time every second
+    #[arg(short = 'c', long = "continuous")]
+    pub continuous: bool,
+
+    /// If set in combination with -c / --continuous, overwrite the previous line instead of printing a new line
+    #[arg(short = 'o', long = "overwrite")]
+    pub overwrite: bool,
+}
--- src/subcommands/clock/run.rs
+++ src/subcommands/clock/run.rs
@@ -1,11 +1,50 @@
+use std::io::stdout;
+use std::thread::sleep;
+use std::time::Duration;
+
+use chrono::{Local, Timelike};
+use crossterm::cursor::MoveLeft;
+use crossterm::execute;
+use crossterm::style::Print;
+use crossterm::terminal::{Clear, ClearType};
+
 use crate::cli::subcmd::RunnableSubCmd;
 use crate::subcommands::ClockArgs;
-use chrono::Local;
+
+const FMT: &str = "%H:%M:%S";
+
+impl ClockArgs {
+    fn get_text(&self) -> (String, u32) {
+        let datetime = Local::now();
+        let nanos = 1_000_000_000 - datetime.time().nanosecond();
+        let now = datetime.format(FMT).to_string();
+        (now, nanos)
+    }
+}
 
 impl RunnableSubCmd for ClockArgs {
     fn run(&self) -> Result<(), String> {
-        let now = Local::now().format("%H:%M:%S");
-        println!("{now}");
+        if self.continuous {
+            loop {
+                let (text, nanos) = self.get_text();
+                let execution = if self.overwrite {
+                    execute!(
+                        stdout(),
+                        Clear(ClearType::CurrentLine),
+                        MoveLeft(100),
+                        Print(text)
+                    )
+                } else {
+                    execute!(stdout(), Print(text), Print("\n"))
+                };
+
+                execution.or_else(|x| Err(x.to_string()))?;
+                sleep(Duration::from_nanos(1 + nanos as u64));
+            }
+        } else {
+            let (text, _) = self.get_text();
+            println!("{text}");
+        }
         Ok(())
     }
 }
